a7aa6382e10668302d309bfc403fb0107c9e779f
Ian <ian.shane.sears@gmail.com>
2020-04-02 00:13:31 -0700

    imported from borked repo

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (40) NOT NULL,
+    devoured TINYINT DEFAULT 0,
+    PRIMARY KEY(id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured)
+VALUES
+  ("Burg", 0),
+  ("Hamburg", 0),
+  ("Big Boi", 0),
+  ("Lidl Boi", 1),
+  ("Thing", 1),
+   ("Other Thing", 1);
